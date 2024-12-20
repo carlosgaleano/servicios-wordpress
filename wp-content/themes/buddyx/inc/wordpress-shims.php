9ca974887e9fcd15a2440dbb791ac3b8781cc686
carlosgaleano <59021022+carlosgaleano@users.noreply.github.com>
2024-12-20 18:06:39 -0300

    first commit

---
+++ wp-content/themes/buddyx/inc/wordpress-shims.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Shims for recent WordPress functions.
+ */
+
+/*
+ * Adds backwards compatibility for buddyx_wp_body_open() introduced with WordPress 5.2
+ */
+if ( ! function_exists( 'buddyx_wp_body_open' ) ) {
+	/**
+	 * Run the buddyx_wp_body_open action.
+	 *
+	 * @return void
+	 */
+	function buddyx_wp_body_open() {
+		do_action( 'buddyx_wp_body_open' );
+	}
+}
